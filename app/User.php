13858fa513f89dc33e260805e300688354b18e53
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-15 12:15:41 -0600

    created a 'owns' method to check if user owns the post and update the post

--- app/User.php
+++ app/User.php
@@ -26,4 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function owns($related) {
+        return $this->id == $related->user_id;
+    }
 }
