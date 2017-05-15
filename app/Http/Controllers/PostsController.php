8fec2a59e0fde812593fe1cfabd479976da713bb
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-15 12:12:14 -0600

    created a post controller

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Gate;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function show($id)
+    {
+    	// auth()->logout();
+    	auth()->loginUsingId(1); //temp
+
+    	$post = Post::findOrFail($id);
+
+    	// $this->authorize('show-post', $post);
+
+    	// if (Gate::denies('show-post', $post)) {
+    	// 	abort(403, 'Sorry, not sorry.');
+    	// }
+    	
+    	Auth::user()->can('update-post', $post);
+
+    	return view('posts.show', compact('post'));
+    }
+}
