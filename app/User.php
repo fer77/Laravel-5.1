82135a7f731a2618455f77eebdc96f93cd2713c9
Fernando Fernandez <ffmp777@yahoo.com>
2017-05-16 09:00:19 -0600

    created permissions, roles, and assigned them to users

--- app/User.php
+++ app/User.php
@@ -29,4 +29,24 @@ class User extends Authenticatable
     public function owns($related) {
         return $this->id == $related->user_id;
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function assignRole($role)
+    {
+        return $this->roles()->save(
+            Role::whereName($role)->firstOrFail());
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        return !! $role->intersect($this->roles)->count();
+    }
 }
